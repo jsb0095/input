de6849be0bcafe6b473949fcf875870ef95a461b
jsb0095 <anfzhd0095@gmail.com>
2022-09-07 17:58:10 +0900

    model 수정

--- lib/Member1.dart
+++ lib/Member1.dart
@@ -3,48 +3,48 @@ import 'dart:core';
 import 'package:json_annotation/json_annotation.dart';
 
 class Member1 {
-  final String memberId;
-  final String memberPw;
-  final String memberPwc;
-  final String memberEmail;
-  final String memberName;
-  final String memberAddress1;
-  final String memberAddress2;
-  final String memberAddress3;
-  final String memberAddress4;
-  final String memberBirthday;
-  final String memberGender;
-  final String memberPhone;
-  @JsonSerializable()
+  final String id;
+  final String pw;
+  final String pwc;
+  final String email;
+  final String name;
+  final String address1;
+  final String address2;
+  final String address3;
+  final String address4;
+  final String birthday;
+  final String gender;
+  final String phone;
+
   Member1(
-      {required this.memberId,
-      required this.memberPw,
-      required this.memberPwc,
-      required this.memberEmail,
-      required this.memberName,
-      required this.memberAddress1,
-      required this.memberAddress2,
-      required this.memberAddress3,
-      required this.memberAddress4,
-      required this.memberBirthday,
-      required this.memberGender,
-      required this.memberPhone});
+      {required this.id,
+      required this.pw,
+      required this.pwc,
+      required this.email,
+      required this.name,
+      required this.address1,
+      required this.address2,
+      required this.address3,
+      required this.address4,
+      required this.birthday,
+      required this.gender,
+      required this.phone});
 
   factory Member1.formMap(Map member1Map) {
     var memberMap;
     return Member1(
-      memberId: memberMap['memberId'],
-      memberPw: memberMap['memberPw'],
-      memberPwc: memberMap['memberPwc'],
-      memberEmail: memberMap['memberEmail'],
-      memberName: memberMap['memberName'],
-      memberAddress1: memberMap['memberAddress1'],
-      memberAddress2: memberMap['memberAddress2'],
-      memberAddress3: memberMap['memberAddress3'],
-      memberAddress4: memberMap['memberAddress4'],
-      memberBirthday: memberMap['memberBirthday'],
-      memberGender: memberMap['memberGender'],
-      memberPhone: memberMap['memberPhone'],
+      id: memberMap['id'],
+      pw: memberMap['pw'],
+      pwc: memberMap['pwc'],
+      email: memberMap['email'],
+      name: memberMap['name'],
+      address1: memberMap['address1'],
+      address2: memberMap['address2'],
+      address3: memberMap['address3'],
+      address4: memberMap['address4'],
+      birthday: memberMap['birthday'],
+      gender: memberMap['gender'],
+      phone: memberMap['phone'],
     );
   }
 }
